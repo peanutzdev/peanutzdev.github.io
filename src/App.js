76fa6caf0a4fdbbbc5932be92b2a5892269a2220
peanutzdev <project0x@protonmail.com>
2023-07-06 07:09:11 +0300

    Updating fav icon, switching to coming soon

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import IntroSection from "./components/IntoSection";
 import TokenomicsSection from "./components/TokenomicsSection";
 import RoadmapSection from "./components/RoadmapSection";
 import Footer from "./components/Footer";
+import ComingSoon from "./components/ComingSoon";
 
 function App() {
   return (
     <div className="App">
       <div className="contentWrapper">
-        <Header/>
-        <Hero/>
-        <IntroSection/>
-        <TokenomicsSection/>
-        <RoadmapSection/>
+        {/*<Header/>*/}
+        {/*<Hero/>*/}
+        {/*<IntroSection/>*/}
+        {/*<TokenomicsSection/>*/}
+        {/*<RoadmapSection/>*/}
+        <ComingSoon/>
       </div>
-      <Footer/>
+      {/*<Footer/>*/}
     </div>
   );
 }
